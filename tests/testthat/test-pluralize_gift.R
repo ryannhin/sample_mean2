4856be34113a4a3b5183a207d8369d08a1352eb8
ryannhin <73153513+ryannhin@users.noreply.github.com>
2021-05-23 22:56:52 -0700

    Delete tests/testthat directory

--- tests/testthat/test-pluralize_gift.R
+++
@@ -1,6 +0,0 @@
-context("pluralize gift")
-
-test_that("correct output", {
-  expect_equal(pluralize_gift("cat"), "cats")
-  expect_equal(pluralize_gift("bunny"), "bunnies")
-})
--- tests/testthat/test-sing_day.R
+++
@@ -1,6 +0,0 @@
-context("sing day")
-
-test_that("throw an error",{
-  expect_error(sing_day(xmas, 2, Full.Phrase,2))
-
-})
